23bd59ea34a0cd25b4ffdf467a69379cb0f26141
Sylvain Galtier <sylvaingaltier@gmail.com>
2022-08-16 17:21:31 +0200

    create model pet with validations and routes for pets

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,20 @@
+class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
